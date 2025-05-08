cf10d585344b1717cbae672f215c925f5916e051
André Lira <andrelira50@gmail.com>
2025-05-08 16:55:42 -0300

    adicionei o GET e DELETE mas este ainda n funciona

--- app.py
+++ app.py
@@ -14,5 +14,23 @@ def create():
     }
     return flask.jsonify(response), 200
 
+@app.route("/tasks", methods=['GET'])
+def getTasks():
+    tasks = manager.GetTasks()
+    response = []
+    for task in tasks:
+        newItem = {
+            "id": task.getID() ,
+            "author": task.getAuthor(),
+            "task": task.getTask()
+        }
+        response.append(newItem)
+    return flask.jsonify(response) 
+
+@app.route("/task", methods=['DELETE'])
+def delTasks():
+    values = flask.request.get_json()
+    manager.deleteTask(values.get('id'))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
